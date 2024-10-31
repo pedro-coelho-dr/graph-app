6522f35c55a195d3d088977941a9da177acac5f3
Kal-0 <106926790+Kal-0@users.noreply.github.com>
2024-10-31 10:36:30 -0300

    start do projeto

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("RelaxAresta")
+import streamlit as st
+from pages import test_page as tp
+# Dicionário de páginas com as referências
+pages = {
+    "Página 1": tp,
+}
+
+# Menu lateral
+st.sidebar.title("Menu")
+page = st.sidebar.selectbox("Selecione a página", options=list(pages.keys()))
+
+# Carregar a página selecionada
+selected_page = pages[page]
+selected_page.app()
---
+++ pages/test_page.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title("Página 1")
+    st.write("Bem-vindo à Página 1")
